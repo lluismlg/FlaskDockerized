a3e9d61b18ec8e3effcbaabec28cbf56e927dc48
lluis <33216794+lluismlg@users.noreply.github.com>
2021-08-04 15:37:27 +0200

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,4 @@ def hello(name=None):
     return render_template('hello.html', name=name)
     
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
